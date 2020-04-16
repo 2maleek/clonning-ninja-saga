e8280f856c213f0d800cb92de26849acf37a93a8
Evandy <andi_evandy@yahoo.co.id>
2020-04-16 20:01:38 +0700

    client: game room test

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,17 +2,20 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <RoomTest></RoomTest>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
+import RoomTest from './components/RoomTest.vue';
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
-  },
+    RoomTest
+  }
 };
 </script>
 
--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
 
+import VueSocketIO from 'vue-socket.io'
+
+Vue.use(new VueSocketIO({
+  connection: 'http://localhost:3000'
+}));
+
 Vue.config.productionTip = false;
 
 new Vue({
