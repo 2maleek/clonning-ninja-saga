cd8e9a881b8d6a64a83afe614a47340cfc58a904
Ignatius Agus <ignatiusagus@Ignatiuss-MacBook-Pro.local>
2020-04-16 21:00:01 +0700

    create function to reduce power create dummy appGame  to see if socket can transmit data

--- client/src/components/Gameboard.vue
+++ client/src/components/Gameboard.vue
@@ -2,7 +2,6 @@
   <div>
     <h1>TITLE: "CLONNING NINJA SAGA</h1>
     <div class="arena">
-        <div class="winnerwarning"></div>
         <div>
             <h3>Player 1</h3>
             <div class="modeAttack">{{attack1}}</div>
@@ -10,6 +9,7 @@
             <div class="avatar">Icon1</div>
             <div class="power">{{power1}}</div>
         </div>
+        <div class="winnerwarning"></div>
         <div>
             <h3>Player 2</h3>
             <div class="modeAttack">{{attack2}}</div>
@@ -36,7 +36,7 @@
                 <input type="radio" value="fire" v-model="elementDefense"><label>Fire</label>
                 <input type="radio" value="water" v-model="elementDefense"><label>Water</label>
                 <input type="radio" value="soil" v-model="elementDefense"><label>Soil</label>
-                <button>Defense</button>
+                <button @click="sendDefense">Defense</button>
             </form>
         </div>
     </div>
@@ -45,14 +45,18 @@
 </template>
 
 <script>
-
+import io from 'socket.io-client';
+const socket = io('http://localhost:3000');//jangan lupa diganti saat deploy
 export default {
   data(){
     return {
-        power: 100,
+        power1: 100,
+        power2: 100,
         player: '',
-        attack: false,
-        defense: false,
+        attack1: '',
+        attack2: '',
+        defense1: '',
+        defense2: '',
         elementAttack: '',
         elementDefense: '',
         msgAttack : null,
@@ -62,14 +66,34 @@ export default {
   created: function(){
       console.log('created detected')
       socket = io()
-      socket.on('msgAttack', function(attackOp){
-          console.log(attackOp, "attackOp on")
-
+      socket.on('msgAttack', function(attack){
+          console.log(attack, "......attack")
+          if(attack.player === 'player1'){
+              this.attack1 = attack.elementAttack
+              
+          } else {
+              this.attack2 = attack.elementAttack
+          }
+      })
+      socket.on('msgDefense', function(defense){
+          console.log(defense, "....defense")
+          if(defense.player === 'player1'){
+              this.defense1 = defense.elementDefense
+          } else {
+              this.defense2 = defense.elementDefense
+          }
       })
   },
   methods:{
     sendAttack (event){
         event.preventDefault();
+        //mengisi attack sendiri
+        if(this.player === 'player1'){
+            this.attack1 = this.elementAttack
+        } else {
+            this.attack2 = this.elementAttack
+        }
+        //menyiapkan message untuk socket
         this.msgAttack = {
             player: this.player,
             elementAttack: this.elementAttack
@@ -79,6 +103,13 @@ export default {
     },
     sendDefense (event){
         event.preventDefault();
+        //mengisi desfense sendiri
+        if(this.player === 'player1'){
+            defense1 = this.elementDefense
+        } else {
+            defense2 = this.elementDefense
+        }
+        //menyiapkan msg untuk socket
         this.msgDefense = {
             player: this.player,
             elementDefense: this.elementDefense
@@ -93,5 +124,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.arena{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+}
 </style>
---
+++ server/appGame.js
@@ -0,0 +1,29 @@
+var app = require('express')();
+const socket = require('socket.io')
+const port = process.env.PORT || 3000
+let server = app.listen(port, () => console.log('listening on port: ', port))
+
+// app.get('/', function(req, res){
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+// io.on('connection', function(socket){
+//   console.log('a user connected');
+//   socket.on('disconnect', function(){
+//     console.log('user disconnected');
+//   });
+// });
+var io = socket(server)
+io.on('connection', function(socket){
+  console.log('someone connected')
+    socket.on('msgAttack', function(msg){
+      console.log(msg, "attack")
+      //io.emit('msgAttack', msg) //ke semua
+      socket.broadcast.emit('msgAttack', msg) //ke semua kecuali diri sendiri
+     });
+    socket.on('msgDefense', function(msg){
+      console.log(msg, "defense")
+      //io.emit('msgDefense', msg) //ke semua
+      socket.broadcast.emit('msgDefense', msg) //ke semua kecuali diri sendiri
+     });
+  });
