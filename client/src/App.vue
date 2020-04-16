44d2b9b2a530048d161c2cf6066d21916bf4cba8
Ignatius Agus <ignatiusagus@Ignatiuss-MacBook-Pro.local>
2020-04-16 17:34:31 +0700

    create component Gameboard start tempate start script

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+     <Gameboard/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+//  import HelloWorld from './components/HelloWorld.vue';
+import Gameboard from './components/Gameboard.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    //  HelloWorld,
+    Gameboard,
   },
 };
 </script>
---
+++ client/src/components/Gameboard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h1>TITLE: "CLONNING NINJA SAGA</h1>
+    <div class="arena">
+        <div class="winnerwarning"></div>
+        <div>
+            <h3>Player 1</h3>
+            <div class="modeAttack"></div>
+            <div class="modeDefense"></div>
+            <div class="avatar"></div>
+            <div class="power"></div>
+        </div>
+        <div>
+            <h3>Player 2</h3>
+            <div class="modeAttack"></div>
+            <div class="modeDefense"></div>
+            <div class="avatar"></div>
+            <div class="power"></div>
+        </div>
+    </div>
+
+    <div class="privateScreen">
+        <div class="warningReady"></div>
+        <div class="chooseAttack"></div>
+        <div class="chooseDefense"></div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+export default{
+    data()
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
